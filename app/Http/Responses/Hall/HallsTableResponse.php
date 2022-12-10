a243d37092accf2a034427a2d1323a8c8d6bccc3
Lukas Velička <lukasv093@gmail.com>
2022-12-10 16:47:16 +0200

    progress

--- app/Http/Responses/Hall/HallsTableResponse.php
+++ app/Http/Responses/Hall/HallsTableResponse.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Responses\Hall;
 
+use App\Models\Division;
+use App\Models\Hall;
 use Spatie\LaravelData\Data;
 
 final class HallsTableResponse extends Data
@@ -9,9 +11,27 @@ final class HallsTableResponse extends Data
     public function __construct(
         public int $id,
         public int $division_id,
+        public string $division_address,
         public string $name,
         public int $seats_count,
     )
     {
+    }
+
+    /**
+     * @param Hall $payload
+     * @return static
+     */
+    public static function fromModel($payload): static
+    {
+        $divisionAddress = Division::query()->where('id', $payload->division_id)->pluck('address');
+
+        return new self(
+            id: $payload->getKey(),
+            division_id: $payload->division_id,
+            division_address: $divisionAddress,
+            name: $payload->name,
+            seats_count: $payload->seats_count,
+        );
     }
 }
